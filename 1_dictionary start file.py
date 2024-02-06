f6ee97cf73952b83f94f80ba7a12bf044a230373
Megan Monthofer <meganmonthofer@gmail.com>
2024-02-06 13:41:56 -0600

    learning dict

--- 1_dictionary start file.py
+++ 1_dictionary start file.py
@@ -4,93 +4,82 @@ phonebook = {'Chris':'555−1111',
              'Katie':'555−2222',
              'Joanne':'555−3333'}
 
-
+'''
 
 print()
 print('*****  start section 1 - print dictionary ********')
 print()
 
+print(phonebook, '\n')
+print(len(phonebook))
 
+mydictionary{}
 
-
+mydictionary = dict(m=8, n=9)
 
 print()
 print('*****  end section 1 ********')
 print()
-
-
-'''
-
 
 print()
 print('*****  start section 2 - search dictionary ********')
 print()
 
 
-
-
-
+name = 'chris'
+if name in phonebook:
+    print(f"Name: {name} Phone Number: {phonebook[name]}")
+else:
+    print(f"{name} is not in the phone book")
 
 
 print()
 print('*****  end section 2 ********')
 print()
 
-
-
-
-
-
-
 print()
 print('*****  start section 3 - edit/append dictionary ********')
 print()
 
+print(phonebook)
 
+phonebook['Joe'] = '555-0123'
+phonebook['Chris'] = '555-4444'
 
-
+print(phonebook)
 
 print()
 print('*****  end section 3 ********')
 print()
-
-
-
-
-
 
 print()
 print('*****  start section 4 - delete/remove from dictionary ********')
 print()
 
 
+print(phonebook)
+del phonebook['Chris']
+print(phonebook)
 
 
 print()
 print('*****  end section 4 ********')
 print()
-
-
-
-
-
-
+'''
 print()
 print('*****  start section 5 - iterate through keys, values, items ********')
 print()
 
 
+for key in phonebook:
+    print(f"Name: {key} Phone Number: {phonebook[key]}")
 
-
-
+for values in phonebook.values():
+    print(values)
 
 print()
 print('*****  end section 5 ********')
 print()
-
-
-
-
 
 print()
 print('*****  start section 6 - using get and clear ********')
@@ -150,7 +139,7 @@ print('*****  end section 9 ********')
 print()
 
 
-'''
+
 
 
 
