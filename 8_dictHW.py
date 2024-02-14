e342e9c74bf6a183689eb7176410ab9bb1767103
Megan Monthofer <meganmonthofer@gmail.com>
2024-02-13 23:25:05 -0600

    Dict HW Solution

---
+++ 8_dictHW.py
@@ -0,0 +1,24 @@
+#
+
+student = {'name':'Megan Monthofer',
+           'age':21,
+           'major':'MIS',
+           'hobbies':['pickleball', 'chess', 'volunteering']}
+
+student['State'] = 'Texas'
+
+student['age'] += 1
+
+for k, v in student.items():
+    if(k == 'hobbies'):                             # properly formats hobbies
+        print(f'Your {k} are ', end = '') 
+        j = 0
+        for i in student['hobbies']:
+            if j == len(student['hobbies']) - 1:    # properly prints last element in hobbies
+                print(f'{i}')
+            else:
+                print(f'{i}', end = ', ')           # prints elements in hobbies
+            j += 1
+    else:
+        print(f'Your {k} is {v}')                   # prints elements in student
+
---
+++ 9_dictHW.py
@@ -0,0 +1,23 @@
+#
+def main():
+    movie_ratings = {'IT 2' : 8, 'Howls Moving Castle' : 10, 'The Creator' : 4, 'Willy Wonka' : 0,
+                    'Aqua Man' : 7, 'Home' : 9, 'Holes' : 6, 'Oppenheimer' : 10, 'Steven Universe' : 6,
+                    'Spider Man No Way Home' : 5, 'Toy Story' : 8, 'Scary Movie' : 3, 'Space Jam' : 9}
+
+    movie = input('Enter a movie title: ')
+
+    recommend_movie(movie_ratings, movie)
+
+def recommend_movie(movie_ratings, movie):
+    if(movie in movie_ratings and movie_ratings[movie] >= 8):
+        print(f'I recommend {movie} - rated {movie_ratings[movie]}/10')
+    else:
+        print(f'I recommend these movies:')
+        for k, v in movie_ratings.items():
+            if v >= 8:
+                print(f'{k} - rated {v}/10')
+                
+
+
+if __name__ == "__main__":
+    main()
